724c8fb6bde44ba1feddb964da6449bd5ac066d8
Neeraj Gir <neerajgoswami871@gmail.com>
2026-07-27 01:38:39 +0500

    feat: add Footer component and styles to App, including footer text

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Nav from "./components/Nav"
 import './App.css'
 import Hero from './components/Hero';
+import Footer from './components/Footer';
 const App = () => {
   return (
     <>
       <Nav/>
       <Hero/>
+      <Footer/>
     </>
   )
 }
