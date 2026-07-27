e2e3e2bdfc418e93b77515fc3954dc25781eaff2
Neeraj Gir <neerajgoswami871@gmail.com>
2026-07-28 00:32:44 +0500

    feat: update Useeffect component to manage two separate count states and modify rendering logic

--- src/components/Useeffect.jsx
+++ src/components/Useeffect.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from 'react'
 
 const Useeffect = () => {
-    let [count, setCount] = useState(0)
+    let [count1, setCount1] = useState(0)
+    let [count2, setCount2] = useState(0)
     useEffect(()=>{
-        setTimeout(() => {
-            setCount(++count)
-        }, 2000);
-    },[count])
+        console.log("Hello World")
+    },[count1])
   return (
     <div>
-        <h1>{count}</h1>
+        <h1>{count1}</h1>
+        <h1>{count2}</h1>
+        <button onClick={()=>setCount1(++count1)}>BTN</button>
+        <button onClick={()=>setCount2(++count2)}>BTN</button>
     </div>
   )
 }
