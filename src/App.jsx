97b3ac9880daca1842bc8ca3412127c2455cca77
Neeraj Gir <neerajgoswami871@gmail.com>
2026-07-27 22:56:10 +0500

    feat: add Props component and update styles in App and index.css for layout and design

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Nav from "./components/Nav"
 import './App.css'
 import Hero from './components/Hero';
 import Footer from './components/Footer';
+import Props from "./components/Props"
 const App = () => {
   return (
     <>
-      <Nav/>
-      <Hero/>
-      <Footer/>
+      {/* <Nav/> */}
+      {/* <Hero/> */}
+      {/* <Footer/> */}
+      <Props name={"Neeraj"}/>
     </>
   )
 }
---
+++ src/components/Props.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Props = (props) => {
+  return (
+    <div className="props">
+        <h1>Props</h1>
+        <p>Hello Myself {props.name}</p>
+    </div>
+  )
+}
+
+export default Props
